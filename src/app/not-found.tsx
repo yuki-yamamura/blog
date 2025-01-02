b1a6ad36171a684f0aec7e40d58f9306ef08f3a8
Yuki Yamamura <yuki.yamamura@icloud.com>
2025-01-02 17:52:16 +0900

    add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import { yomogi } from '@/lib/next';
+
+import styles from './not-found.module.css';
+
+const Page = () => (
+  <div className={styles.base}>
+    <h1 className={`${yomogi.className} ${styles.heading}`}>404 Not Found</h1>
+  </div>
+);
+
+export default Page;
