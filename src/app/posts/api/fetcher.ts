f3f4305b0c0b17d62e93e9ee3728015fb2fae5d6
Yuki Yamamura <yuki.yamamura@icloud.com>
2025-01-03 07:02:52 +0900

    refactor: remove unused function

--- src/app/posts/api/fetcher.ts
+++ src/app/posts/api/fetcher.ts
@@ -1,17 +1,9 @@
 import { client } from '@/lib/microcms';
 
 import type { Post } from '../types';
-import type { GetAllContentIdsRequest, GetListDetailRequest } from 'microcms-js-sdk';
-
+import type { GetAllContentIdsRequest } from 'microcms-js-sdk';
 
 const endpoint = 'posts';
-
-export const getPost = async (params: Omit<GetListDetailRequest, 'endpoint'>) => {
-  return client.getListDetail<Post>({
-    ...params,
-    endpoint,
-  });
-};
 
 export const getPosts = async (params?: Omit<GetAllContentIdsRequest, 'endpoint'>) => {
   return client.getAllContents<Post>({
