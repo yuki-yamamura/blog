f3e074d29a9c136f998c5a7efe40008b1778fbf7
Yuki Yamamura <yuki.yamamura@icloud.com>
2025-01-02 10:11:29 +0900

    configure ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import importPlugin from 'eslint-plugin-import';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,49 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'import/newline-after-import': 'error',
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+            'type',
+            'unknown',
+          ],
+          pathGroups: [
+            {
+              pattern: '*.css',
+              group: 'unknown',
+              patternOptions: { matchBase: true },
+              position: 'after',
+            },
+          ],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          'newlines-between': 'always',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          args: 'after-used',
+          argsIgnorePattern: '_',
+          ignoreRestSiblings: false,
+          varsIgnorePattern: '_',
+        },
+      ],
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
 ];
 
 export default eslintConfig;
