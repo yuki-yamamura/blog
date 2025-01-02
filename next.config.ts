0c428038d14276a4aaf1d0e034f0f56b49a17a38
Yuki Yamamura <yuki.yamamura@icloud.com>
2025-01-02 21:08:01 +0900

    use next.config.js for redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: 'export',
+  redirects: async () => [
+    {
+      source: '/posts',
+      destination: '/posts/page/1',
+      permanent: true,
+    },
+  ],
 };
 
 export default nextConfig;
