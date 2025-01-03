26878a6a375ccbfe4fe93fb362ae4c365ea1f01c
Yuki Yamamura <yuki.yamamura@icloud.com>
2025-01-03 16:13:24 +0900

    fix redirects

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  redirects: async () => [
-    {
-      source: '/posts',
-      destination: '/posts/page/1',
-      permanent: true,
-    },
-  ],
 };
 
 export default nextConfig;
