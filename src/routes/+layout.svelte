824fbeea4cfc8322f5752f96df910acd4f5664f3
Yuki Yamamura <126521201+yuki-yamamura@users.noreply.github.com>
2026-07-26 05:56:39 +0900

    use Nunito as primary font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/components/layouts/Header.svelte';
 	import WindChime from '$lib/components/layouts/WindChime.svelte';
+	import '@fontsource-variable/nunito/wght.css';
 	import '../styles/reset.css';
 
 	let { children } = $props();
@@ -18,6 +19,10 @@
 </div>
 
 <style>
+	:global(body) {
+		font-family: 'Nunito Variable', sans-serif;
+	}
+
 	.base {
 		margin-inline: auto;
 		max-width: 1280px;
