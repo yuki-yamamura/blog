2df2daacc6425fb0ca71f5177d308f80974defe8
Yuki Yamamura <126521201+yuki-yamamura@users.noreply.github.com>
2026-07-26 20:16:15 +0900

    fix: use srcset for Retina display (#2)

--- src/lib/components/ui/Image.svelte
+++ src/lib/components/ui/Image.svelte
@@ -1,19 +1,32 @@
 <script lang="ts">
-	import type { HTMLImgAttributes } from 'svelte/elements';
-	import type { AllNonNullable } from '$lib/types/all-non-nullable';
-	import { page } from '$app/state';
-	import { transformImageUrl } from '../../utils/image';
+	import { transformImageUrl } from '$lib/utils/image';
 
-	const props: AllNonNullable<Pick<HTMLImgAttributes, 'src' | 'alt' | 'width' | 'height'>> &
-		Omit<HTMLImgAttributes, 'src' | 'alt' | 'width' | 'height'> = $props();
-	const optimizedImageUrl = $derived(
+	import type { AllNonNullable } from '$lib/types/all-non-nullable';
+	import type { HTMLImgAttributes } from 'svelte/elements';
+
+	import { page } from '$app/state';
+
+	const props: AllNonNullable<Pick<HTMLImgAttributes, 'alt' | 'height' | 'src' | 'width'>> &
+		Omit<HTMLImgAttributes, 'alt' | 'height' | 'src' | 'width'> = $props();
+
+	const hostname = $derived(page.url.hostname);
+	const src1x = $derived(
 		transformImageUrl({
+			height: Number(props.height),
+			hostname,
 			src: props.src,
-			width: props.width,
-			height: props.height,
-			hostname: page.url.hostname
+			width: Number(props.width)
 		})
 	);
+	const src2x = $derived(
+		transformImageUrl({
+			height: Number(props.height) * 2,
+			hostname,
+			src: props.src,
+			width: Number(props.width) * 2
+		})
+	);
+	const srcset = $derived(`${src2x} 2x`);
 </script>
 
-<img {...props} src={optimizedImageUrl} />
+<img {...props} src={src1x} {srcset} />
